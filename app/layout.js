f898f3a229cf7492a57eda286f03b0d5b5b4d574
Kartikeya Kesarwani <kartikeyakesarwaniv@gmail.com>
2025-07-10 22:33:40 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,31 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import { Outfit } from "next/font/google";
+import "./globals.css";
+import { AppContextProvider } from "@/context/AppContext";
+import { Toaster } from "react-hot-toast";
+
+const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+
+export const metadata = {
+  title: "ElectroCart",
+  description: "E-Commerce with Next.js ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${outfit.className} antialiased text-gray-700`}
+        data-new-gr-c-s-checked-loaded='14.1209.0'
+        data-gr-ext-installed=''
+      
+       >
+          <Toaster />
+          <AppContextProvider>
+            {children}
+          </AppContextProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
