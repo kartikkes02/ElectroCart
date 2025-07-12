00d323e369e0a150a655b4f0d764bbd96a075259
Kartikeya Kesarwani <163706267+kartikkes02@users.noreply.github.com>
2025-07-12 10:40:37 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
-import { unique } from "next/dist/build/utils";
 
 const userSchema = new mongoose.Schema({
-    _id:{type: String, required: true},
-    name:{type: String, required: true},
-    email:{type: String, required: true, unique:true},
-    imageURL:{type: String, required: true},
-    cartItems:{type:Object, default:{}},
-
-}, {minimize: false});
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  imageURL: { type: String, required: true },
+  cartItems: { type: Object, default: {} },
+}, { minimize: false });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
