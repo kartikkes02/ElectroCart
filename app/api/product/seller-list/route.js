1d684263f4fca114e909243807ff5cf50db000b8
Kartikeya Kesarwani <163706267+kartikkes02@users.noreply.github.com>
2025-07-19 14:19:28 +0530

    Update route.js

--- app/api/product/seller-list/route.js
+++ app/api/product/seller-list/route.js
@@ -1,6 +1,7 @@
 import authSeller from "@/lib/authSeller";
-import { getAuth } from "@clerk/nextjs";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import { connectDB } from "@/config/db";
 
 export async function GET(request) {
     try {
