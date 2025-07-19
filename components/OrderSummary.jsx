ca9b0100d4b156fe188b207dd1d25e7df61f1aeb
Kartikeya Kesarwani <kartikeyakesarwaniv@gmail.com>
2025-07-19 16:12:04 +0530

    Finished Project

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -35,7 +35,32 @@ const OrderSummary = () => {
   };
 
   const createOrder = async () => {
-
+    try {
+      if(!selectedAddress) {
+        return toast.error("Please select an address");
+      }
+      let cartItemsArray = Object.keys(cartItems).map((key) => ({product: key, quantity: cartItems[key]}));
+      cartItemsArray = cartItemsArray.filter(item => item.quantity > 0);
+      if (cartItemsArray.length === 0) {
+        return toast.error("Your cart is empty");
+      }
+      const token = await getToken();
+      const {data} = await axios.post('/api/order/create',{
+        items: cartItemsArray,
+        address: selectedAddress._id,
+      }, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      if( data.success) {
+        toast.success(data.message);
+        setCartItems({});
+        router.push('/order-placed');
+      } else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   }
 
   useEffect(() => {
